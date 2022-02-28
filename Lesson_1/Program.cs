f0eb3024149b904ab347fa95f15e16d7a0d41aff
BogdanChyornyy <booggiman@mail.ru>
2022-02-28 21:30:18 +0300

    Выполнил домашнее задание по уроку 1

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1_enter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите имя пользователя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите текущую дату: ");
+            string date = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + ", сегодня " + date + ".");
+        }
+    }
+}
